b72760100e97bb64b77ce0f3816f962eaf991491
jstonedev <jstonedev521@gmail.com>
2020-10-16 18:46:46 -0400

    update: import GraphQL server

--- src/index.js
+++ src/index.js
@@ -3,9 +3,26 @@ import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./components/App";
 
+// Apollo dependencies
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+// connect ApolloClient
+const httpLink = createHttpLink({
+	uri: "http://localhost:4000",
+});
+
+// Instantiate ApolloClient
+const client = new ApolloClient({
+	link: httpLink,
+	cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
-	<React.StrictMode>
+	<ApolloProvider client={client}>
 		<App />
-	</React.StrictMode>,
+	</ApolloProvider>,
 	document.getElementById("root")
 );
